96e41e52575c6a89f038b2443ca289e799a0f7af
= <=>
2022-01-12 11:31:15 -0600

    updated login backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,21 @@ const resolvers = {
       helloWorld: () => {
         return 'Hello world!';
       }
-    }
-  };
+    },
+
+    Mutation: {
+      //add user mutation
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      
+      login: async () => {
+
+      }
+  }
+};
   
   module.exports = resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,19 @@ const typeDefs = gql`
   type Query {
     helloWorld: String
   }
+
+  type User {
+     _id: ID
+     firstName: String
+     lastName: String
+     email: String
+     orders: [Order]
+   }
+
+   type Auth {
+    token: ID
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
