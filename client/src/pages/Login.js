a5d48ac2af8703c3dd30feedc778db391253b7dd
= <=>
2022-01-11 18:21:19 -0600

    created sign-up page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -20,8 +20,8 @@ function Login(props) {
             const mutationResponse = await login({
                 variables: { email: formState.email, password: formState.password },
             });
-            const token = mutationResponse.data.login.token;
-            Auth.login(token);
+            // const token = mutationResponse.data.login.token;
+            // Auth.login(token);
         } catch (e) {
             console.log(e);
         }
