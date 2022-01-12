dfe90ca493cc647f2cddd0ff773bf603935d3e61
Ghidorah <jljohnson1454@gmail.com>
2022-01-12 10:21:15 -0600

    setting up models

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,2 +1,23 @@
 // purchase date
 // tickets
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    },
+    tickets: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Tickets'
+        }
+    ]
+});
+
+const Tickets = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
