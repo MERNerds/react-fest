2351f821a58439eae1a2c582a93b501e511e330c
Rick Martin <rmartin1985@att.net>
2022-01-11 18:47:28 -0600

    basic graphql setup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,10 @@
-//starter file
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
