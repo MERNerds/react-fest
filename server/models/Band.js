4502a023e88fd2b79a5ee64675d3c736bf4d94b2
Rick Martin <rmartin1985@att.net>
2022-01-12 18:05:54 -0600

    updated models

---
+++ server/models/Band.js
@@ -0,0 +1,39 @@
+// band name
+// set time start
+// set time end
+// date they play
+// _id
+// stage
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const bandSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    stage: {
+        type: String,
+        required: true,
+    }
+
+});
+
+const Band = mongoose.model('Band', bandSchema);
+
+module.exports = Band;
--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,8 @@
 //Starter file
+
+const User = require('./User');
+const Ticket = require('./Ticket');
+const Order = require('./Order')
+const Band = require('./Band');
+
+module.exports = { User, Ticket, Order, Band };
