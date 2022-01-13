6224c3cb8d4dd4fdbbaddd7efda5435a5263c0f6
KyleFerg <kferguson52@gmail.com>
2022-01-12 18:17:55 -0600

    home page that renders hello world

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
+import Home from './pages/Home'
 import './App.css';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   useQuery,
+//   gql
+// } from "@apollo/client";
 
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+// import Login from './pages/Login';
+// import SignUp from './pages/SignUp';
 
 function App() {
   return (
-
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Provider>
-          <Nav />
-          <Switch>  
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/myschedule" component={MySchedule} /> 
-            {/* <Route exact path="/info" component={Info} */}
-          </Switch>
-          </Provider>
-        </div>
-      </Router>
-    </ApolloProvider>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <Home></Home>
   );
 }
 
