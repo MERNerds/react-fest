9aac0dc45e89138eeb347d3659d1d2cd8b37266a
alexzoo0 <alexisvelasquez15@gmail.com>
2022-01-15 12:38:02 -0600

    added QM

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,19 +4,93 @@ import { gql } from "@apollo/client";
 export const QUERY_USER = gql`
   {
     user {
+      _id
       firstName
       lastName
+      email
       orders {
         _id
         purchaseDate
-        products {
+        tickets {
           _id
-          name
+          ticketName
           description
           price
           quantity
         }
       }
+      savedBands {
+        _id
+        bandName
+        startTime
+        endTime
+        stage
+      }
     }
   }
 `;
+
+export const QUERY_USERS = gql`
+{
+  users{
+    _id
+    firstName
+    lastName
+    email
+    savedBands{
+      bandName
+    }
+    order{
+      _id
+    }
+  }
+}
+`;
+
+export const QUER_TICKET = gql`
+{
+  ticket(_id: $_id) {
+    _id
+    ticketName
+    description
+    price
+    quantity
+  }
+}
+`;
+
+export const QUERY_TICKETS = gql`
+{
+  tickets{
+    _id
+    ticketName
+    description
+    price
+    quantity
+  }
+}
+`;
+
+export const QUERY_ORDER = gql`
+{
+  order(_id: $_id) {
+    purchaseDate{
+      _id
+      ticketName
+    }
+  }
+}
+`;
+
+export const QUERY_BANDS = gql`
+{
+  bands{
+    _id
+    bandName
+    startTime
+    endTime
+    date
+  }
+}
+`;
+
