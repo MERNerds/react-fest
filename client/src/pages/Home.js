f4bdda4447ce466c908f7ed9c0d58bab258e0c5a
Rick Martin <rmartin1985@att.net>
2022-01-16 17:52:14 -0600

    removed the queries from the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_BANDS, QUERY_USERS, QUERY_TICKETS } from "../utils/queries";
 // import ProductList from "../components/ProductList";
 // import CategoryMenu from "../components/CategoryMenu";
 // import Cart from '../components/Cart';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_TICKETS);
-  const tickets = data?.tickets || [];
-  console.log(tickets);
   return (
     <div className="container">
       {/* <Lineup />
