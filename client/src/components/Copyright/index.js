167534f111b21992060805b1a7f5d17ab470df03
Kaylee Stevens <kayls@Kaylees-MBP.lan>
2022-01-20 16:51:58 -0600

    removed some code

--- client/src/components/Copyright/index.js
+++ client/src/components/Copyright/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 // copyright
 function Copyright(props) {
     return (
-        <Typography variant="body2" color="text.secondary" align="center" fontSize="36px">
+        <Typography variant="body2" color="text.secondary" align="center">
             {'Copyright © '}
             <Link color="inherit" to="https://mui.com/">
                 React Fest
