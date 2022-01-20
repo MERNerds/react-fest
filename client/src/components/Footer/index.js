47eb0781fd9b39fc1df4ec8593088e584cc39ef8
Rick Martin <rmartin1985@att.net>
2022-01-19 21:02:21 -0600

    adjusted footer styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,74 +1,47 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import {Container, Grid, Card} from '@mui/material';
-import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom'
 import { Paper } from '@mui/material';
 import { makeStyles } from '@material-ui/core/styles';
-import footer from '../../assets/graphics/footer.png'
-import { borderColor, margin, maxHeight } from '@mui/system';
-import CardMedia from '@mui/material/CardMedia';
 import footerImage from '../../assets/images/react-footer.png';
 
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="text.secondary" sx={{margin:'auto', textAlign: 'center', pt:2}}>
-            {'Copyright © '}
-            <Link color="inherit" to="/">
-                React-fest
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
-const styles ={
+const useStyles = makeStyles((theme) => ({
     paperContainer: {
-        backgroundImage:`url(${footerImage})`,
+        backgroundImage: `url(${footerImage})`,
         backgroundSize: "cover",
         backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
+        backgroundPosition: 'center',
+        height: '300px'
 
-    },
-    copyRight: {
-        height: "300px"
     }
-};
+}));
 
 
-export default function Footer() {
-    //const classes = useStyles();
+function Footer() {
+    const classes = useStyles();
 
     return (
-        <Paper style={styles.paperContainer}>
-        <Box 
-        sx={{margin: 0, padding: 0}}
-            sx={{
-                py: 0,
-                px: 0,
-                mt: 'auto',
-            }}
-            >
-            <Copyright />
-
-            
-            
-        </Box>
+        <Paper>
+            <Grid
+                container
+                direction='row'
+                justifyContent='center'
+                alignItems='center'
+                className={classes.paperContainer}>
+                <Grid>
+                    <Typography variant="body2" align="center">
+                        {'Copyright © '}
+                        <Link color="inherit" to="https://mui.com/">
+                            React Fest
+                        </Link>{' '}
+                        {new Date().getFullYear()}
+                        {'.'}
+                    </Typography>
+                </Grid>
+            </Grid>
         </Paper>
     );
 }
-{/* <Container maxWidth='xl'>
-<Grid container spacing={5}>
-    <Grid item ex={12}>
-        <Card>
-            <CardMedia
-            component="img"
-            alt="footer"
-            image={footerImage}
-            />
-        </Card>
-    </Grid>
-</Grid>
-</Container> */}
+
+export default Footer;
